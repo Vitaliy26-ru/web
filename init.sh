ee8b75194fb1037e75116f227e83905169a949a8
Vitaliy26-ru <vees28@yandex.ru>
2021-10-13 07:41:56 +0300

    whih_defult

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
